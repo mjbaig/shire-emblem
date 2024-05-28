b8a57d1948f15a57591f7c1d0c5b5c64c4aca711
maz <7367135+mjbaig@users.noreply.github.com>
2024-05-27 23:58:32 -0700

    update debug scenes

--- shire-emblem-main/scripts/scene_manager.gd
+++ shire-emblem-main/scripts/scene_manager.gd
@@ -1,17 +1,18 @@
 extends Node2D
 
+var statefulLibs = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var node = ShireEmblemStatefulLibs.new();
-	node.test();
-	var array: Array[int] = [1, 2, 3, 4];
-	node.set_tile_map(array, 2, 2);
-	print(node.get_player_range());
+	statefulLibs = ShireEmblemStatefulLibs.new();
+	get_tree().change_scene_to_file("res://scenes/debug/debug_level.tscn");
 	
 	#pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	#var array: Array[int] = [1, 2, 3, 4];
+	#statefulLibs.set_tile_map(array, 2, 2);
+	#print(statefulLibs.get_player_range());
 	pass
